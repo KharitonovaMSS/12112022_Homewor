ac3dffb0f4d36390fbca232d319815a4d4250a2c
Maria <mineeva_maria@mail.ru>
2022-11-12 22:43:08 +0300

    ыведены все четные числа в диапазоне

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-while (1 <= a);
+while (a >= 1)
+{
    if (a % 2 == 0)
-    Console.WriteLine(a + ", ");
-    a++;
+      Console.WriteLine(a + ", ");
+a--;
+}
+rev
