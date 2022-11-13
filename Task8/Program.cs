df7d2517628c0dd0c91ae7e457921eb747c33c5e
Maria <mineeva_maria@mail.ru>
2022-11-13 12:03:03 +0300

    Решение выведено в правильном порядке

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,9 +1,10 @@
 ﻿Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
 
-while (a >= 1)
+while (a >= count)
 {
-   if (a % 2 == 0)
-      Console.WriteLine(a + ", ");
-a--;
+   if (count % 2 == 0)
+      Console.WriteLine(count + ", ");
+count ++;
 }
